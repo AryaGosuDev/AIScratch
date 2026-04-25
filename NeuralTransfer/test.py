4e63725cdc7c8a7c5288735a370169908d94497c
AryaGosuDev <karrat.perjia@shucklak.com>
2026-04-24 21:48:31 -0500

    Init push of ws projects

---
+++ NeuralTransfer/test.py
@@ -0,0 +1,6 @@
+import os
+import tensorflow as tf
+
+print("LD_LIBRARY_PATH =", os.environ.get("LD_LIBRARY_PATH"))
+print("Interpreter check OK")
+print("GPUs =", tf.config.list_physical_devices("GPU"))
